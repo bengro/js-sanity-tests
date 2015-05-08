5818b315548d34b8b77329b16d8358e786449401
Katie Moulang <kmoulang@pivotal.io>
2015-05-08 14:07:44 +0100

    inheritance: removed redudancy
    Signed-off-by: Benjamin Grohbiel <bgrohbiel@pivotal.io>

--- src/Inheritance.js
+++ src/Inheritance.js
@@ -12,69 +12,30 @@
         return 'driving a car';
     };
 
-    function Ferrari() {
-        this.breakdown = function () {
-            return 'breaking down';
-        };
-    }
-
-    Ferrari.prototype = Object.create(Car.prototype);
-
-
     describe("inheritance", function () {
-
-        describe("basics", function () {
-            var ferrari = new Ferrari();
-
-            //TODO add a test of constructor property
-            //TODO how could we change the Car property via the redFerrari obj? -> using constructor property
-
-            it("base object follows changes of its prototype", function () {
-                var car = new Car();
-                expect(car.horn).toBeUndefined();
-                Car.prototype.horn = function () {
-                    return "beep";
-                };
-                expect(car.horn()).toBe("beep");
-            });
-
-            it("derived object follows changes of its base object's prototype", function () {
-                Car.prototype.horn = function () {
-                    return "parp";
-                };
-                expect(ferrari.horn()).toBe("parp");
-            });
-
-            it("changing a property on the derived object hides the base object's value of that property", function () {
-                var redFerrari = new Ferrari();
-                expect(redFerrari.color).toBe('green');
-                redFerrari.color = 'red';
-                expect(redFerrari.color).toBe('red');
-            });
-
-            it("derived object follows changes to base object's properties", function () {
-                var newFerrari = new Ferrari();
-                expect(newFerrari.color).toBe('green');
-                Car.prototype.color = 'blue';
-                expect(newFerrari.color).toBe('blue');
-            });
-        });
 
         describe("objects inheriting from a base constructor", function () {
             describe("inheriting with new operator (deprecated)", function () {
                 describe("when not calling the base constructor", function () {
                     function Mini() {}
-
                     Mini.prototype = new Car();
+
+                    it("has constructor methods of the base object", function () {
+                        var mini = new Mini();
+                        expect(mini.steer()).toBe('steering a car');
+                    });
 
                     it("has prototype methods of the base object", function () {
                         var mini = new Mini();
                         expect(mini.drive()).toBe('driving a car');
                     });
 
-                    it("has constructor methods of the base object", function () {
+                    it("has latest prototype methods of the base object", function () {
                         var mini = new Mini();
-                        expect(mini.steer()).toBe('steering a car');
+                        Car.prototype.horn = function () {
+                            return "parp";
+                        };
+                        expect(mini.horn()).toBe("parp");
                     });
 
                     it("is an instance of the base object", function () {
@@ -84,7 +45,6 @@
                         expect(mini instanceof Object).toBeTruthy();
                     });
                 });
-
             });
 
             describe("inheriting with Object.create", function () {
@@ -94,14 +54,22 @@
 
                     Tesla.prototype = Object.create(Car.prototype);
 
+                    it("does not have constructor methods of the base object", function () {
+                        var tesla = new Tesla();
+                        expect(tesla.steer).toBeUndefined();
+                    });
+
                     it("has prototype methods of the base object", function () {
                         var tesla = new Tesla();
                         expect(tesla.drive()).toBe('driving a car');
                     });
 
-                    it("does not have constructor methods of the base object", function () {
+                    it("has latest prototype methods of the base object", function () {
                         var tesla = new Tesla();
-                        expect(tesla.steer).toBeUndefined();
+                        Car.prototype.horn = function () {
+                            return "parp parp";
+                        };
+                        expect(tesla.horn()).toBe("parp parp");
                     });
 
                     it("is an instance of the base object", function () {
@@ -117,14 +85,22 @@
 
                     SmartCar.prototype = Object.create(Car.prototype);
 
+                    it("has constructor methods of the base object", function () {
+                        var smartCar = new SmartCar();
+                        expect(smartCar.steer()).toBe('steering a car');
+                    });
+
                     it("has prototype methods of the base object", function () {
                         var smartCar = new SmartCar();
                         expect(smartCar.drive()).toBe('driving a car');
                     });
 
-                    it("has prototype methods of the base object", function () {
+                    it("has latest prototype methods of the base object", function () {
                         var smartCar = new SmartCar();
-                        expect(smartCar.steer()).toBe('steering a car');
+                        Car.prototype.horn = function () {
+                            return "parp parp parp";
+                        };
+                        expect(smartCar.horn()).toBe("parp parp parp");
                     });
 
                     it("is an instance of the base object", function () {
@@ -140,20 +116,21 @@
                         Car.call(this);
                     }
 
-                    it("does not have prototype methods of the base object", function () {
-                        var taxi = new Taxi();
-                        expect(taxi.drive).toBeUndefined();
-                    });
-
                     it("has constructor methods of the base object", function () {
                         var taxi = new Taxi();
                         expect(taxi.steer()).toBe('steering a car');
+                    });
+
+                    it("does not have prototype methods of the base object", function () {
+                        var taxi = new Taxi();
+                        expect(taxi.drive).toBeUndefined();
                     });
 
                     it("is not an instance of the base object", function () {
                         var taxi = new Taxi();
                         expect(taxi instanceof Car).toBeFalsy();
                     });
+
                 });
             });
         });
